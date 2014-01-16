1b74e40d5f752f32e3e6c3401d33376b718c0dda
Dariusz Raćkowski <drackows@gmail.com>
2014-01-16 01:06:29 +0100

    testNG cd...

---
+++ junit-tests/src/test/java/pl/inpar/javapowertools/testng/GroupBTests.java
@@ -0,0 +1,13 @@
+package pl.inpar.javapowertools.testng;
+
+import org.testng.annotations.Test;
+
+@Test(groups="B")
+public class GroupBTests {
+	
+	@Test
+	public void testOfGroupB() {
+		System.out.println("  testOfGroupB");
+	}
+	
+}
