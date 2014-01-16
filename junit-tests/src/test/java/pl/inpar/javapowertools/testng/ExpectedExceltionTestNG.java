55e9a81a2134f9b132087361aa06b98acc32f963
Dariusz Raćkowski <drackows@gmail.com>
2014-01-16 01:22:56 +0100

    testng - expected exception

---
+++ junit-tests/src/test/java/pl/inpar/javapowertools/testng/ExpectedExceltionTestNG.java
@@ -0,0 +1,12 @@
+package pl.inpar.javapowertools.testng;
+
+import org.testng.annotations.Test;
+
+public class ExpectedExceltionTestNG {
+	
+	@Test(expectedExceptions={NumberFormatException.class})
+	public void numberFormatExcExp() {
+		Long.parseLong("xxx");
+	}
+	
+}
