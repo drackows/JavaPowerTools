51e72e70c12a893678f5dfd9d15ede16a98e6dbe
Dariusz Raćkowski <drackows@gmail.com>
2014-01-19 19:32:59 +0100

    extract testng project

---
+++ testng-tests/src/test/java/pl/inpar/javapowertools/AppTest.java
@@ -0,0 +1,13 @@
+package pl.inpar.javapowertools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
